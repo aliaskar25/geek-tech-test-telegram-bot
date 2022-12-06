2d25984cb1fee1ed3a5da2b1d09a0e3dee1271ac
aliaskar25 <aliaskar.isakov@gmail.com>
2022-12-06 17:56:30 +0600

    small fix

--- heroku_settings.py
+++ heroku_settings.py
@@ -2,7 +2,7 @@ import os
 
 HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME", default='geek-tech-telegram-bot')
 
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com/'
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PATH = f'/webhook/{os.getenv("BOT_TOKEN", "5663202583:AAE78paT0RNgDmNSxvw1zNfIYbzJ-w3yri0")}'
 WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
